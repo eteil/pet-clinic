ba1bdaea924584a81ab4c19376ea785bdfe71243
DEJ <eteilj@gmail.com>
2020-01-29 14:13:47 +0100

    Implement Mapped Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/dej/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.dej.petclinic.services.map;
+
+import com.dej.petclinic.models.Pet;
+import com.dej.petclinic.services.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/dej/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.dej.petclinic.services.map;
+
+import com.dej.petclinic.models.Vet;
+import com.dej.petclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
