95351f50276828e1b1467358d3b7120d5be99b79
DEJ <eteilj@gmail.com>
2020-01-31 12:58:55 +0100

    List all vets on Vet Index page. Closes #15.

--- pet-clinic-web/src/main/java/com/dej/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/dej/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.dej.petclinic.controllers;
 
+import com.dej.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index","/vets/index.html"})
-    public String lisVets(){
+    @Autowired
+    private VetService vetService;
+
+
+    @RequestMapping({"","/","/vets", "/index","/index.html"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
