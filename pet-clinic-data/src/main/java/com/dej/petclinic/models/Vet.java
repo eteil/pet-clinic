583e1099c85239a3f38aa267fabf21e8ef122aa6
DEJ <eteilj@gmail.com>
2020-02-10 13:54:59 +0100

    Create Vet Speciality Entity and associate to Vet. Closes #27.

--- pet-clinic-data/src/main/java/com/dej/petclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com/dej/petclinic/models/Vet.java
@@ -1,5 +1,7 @@
 package com.dej.petclinic.models;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
     public Vet() {
@@ -7,5 +9,15 @@ public class Vet extends Person{
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }
