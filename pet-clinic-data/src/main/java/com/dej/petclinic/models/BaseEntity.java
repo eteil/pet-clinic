a52089a527c0586eb9917b38e5efd0b6786777f8
DEJ <eteilj@gmail.com>
2020-03-02 16:07:20 +0100

    Convert Owners to JPA Entity. Closes #36.

--- pet-clinic-data/src/main/java/com/dej/petclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/dej/petclinic/models/BaseEntity.java
@@ -1,16 +1,16 @@
 package com.dej.petclinic.models;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-@Entity
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
--- pet-clinic-data/src/main/java/com/dej/petclinic/models/Pet.java
+++ pet-clinic-data/src/main/java/com/dej/petclinic/models/Pet.java
@@ -1,13 +1,24 @@
 package com.dej.petclinic.models;
 
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate localDate;
 
     public Pet() {
