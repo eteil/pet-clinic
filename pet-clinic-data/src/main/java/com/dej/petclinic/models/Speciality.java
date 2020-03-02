06d40b7013059749968c378d95e40ae993691532
DEJ <eteilj@gmail.com>
2020-03-02 16:17:13 +0100

    Convert Vets to JPA Entity. Closes #37.

--- pet-clinic-data/src/main/java/com/dej/petclinic/models/Speciality.java
+++ pet-clinic-data/src/main/java/com/dej/petclinic/models/Speciality.java
@@ -1,7 +1,14 @@
 package com.dej.petclinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
     private String name;
 
--- pet-clinic-data/src/main/java/com/dej/petclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com/dej/petclinic/models/Vet.java
@@ -1,8 +1,11 @@
 package com.dej.petclinic.models;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
     public Vet() {
@@ -12,6 +15,12 @@ public class Vet extends Person{
         super(firstName, lastName);
     }
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "spec_id")
+    )
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
