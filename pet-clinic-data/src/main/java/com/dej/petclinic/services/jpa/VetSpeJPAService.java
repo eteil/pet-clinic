d748be5df2a2566b513aa5e5aaa761e1eb99e97b
djoumatch <eteilj@gmail.com>
2020-03-17 18:58:15 +0100

    Added jpa service for Speciality. Closes #44

---
+++ pet-clinic-data/src/main/java/com/dej/petclinic/services/jpa/VetSpeJPAService.java
@@ -0,0 +1,48 @@
+package com.dej.petclinic.services.jpa;
+
+import com.dej.petclinic.models.Speciality;
+import com.dej.petclinic.repositories.SpecialityRepo;
+import com.dej.petclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("spring-data-jpa")
+public class VetSpeJPAService implements SpecialityService {
+
+    private final SpecialityRepo specialityRepo;
+
+    public VetSpeJPAService(SpecialityRepo specialityRepo) {
+        this.specialityRepo = specialityRepo;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepo.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepo.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepo.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepo.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepo.delete(object);
+    }
+}
